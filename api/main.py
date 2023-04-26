d08024faf7cd2f5bcdccd089713d50330a64ae23
Anton Krytskyi <anton_krytskyi@epam.com>
2023-04-26 21:35:58 +0300

    root GET redirect to `api/v1/docs`

--- api/main.py
+++ api/main.py
@@ -1,16 +1,16 @@
 """Contains main app."""
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 
-from api import __version__
-from api.v1 import app as api_v1
+from api.v1 import app as apiv1
 
 app = FastAPI()
 
 API_V1_PATH = "/api/v1"
-app.mount(API_V1_PATH, api_v1)
+app.mount(API_V1_PATH, apiv1)
 
 
 @app.get("/")
 async def root():
     """Get root."""
-    return {"message": f"get API v{__version__} docs on '{API_V1_PATH}/docs'"}
+    return RedirectResponse(url=API_V1_PATH + "/docs")
